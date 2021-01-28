9b6a615c600efc4054ed79541c76e36ee2d0ad06
Your Name <you@example.com>
2021-01-28 15:41:27 +0330

    small chat app

--- chat.js
+++ chat.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
+const socketio = require("socket.io");
 
 app.use(express.static(__dirname + "/public"));
 
-app.listen(9000);
+const expressServer = app.listen(9000);
+
+const io = socketio(expressServer);
+
+io.on("connection", (socket) => {
+  socket.emit("messageFromServer", {
+    data: "welcome to the socketio server :O",
+  });
+  socket.on("messageToServer", (dataFromClient) => {
+    console.log(dataFromClient);
+  });
+
+  socket.on("newMessageToServer", (msg) => {
+    //console.log(msg);
+    io.emit("messageToClients", { text: msg.text });
+  });
+});
